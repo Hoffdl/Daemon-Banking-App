02bbb96c2e24adf966a9b53f5a5c1ff1077ed1f9
Henry <ikegwuonuhenry25@gmail.com>
2023-12-18 11:00:01 +0000

    create acct

---
+++ createAccount.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void createAccount()
+{
+/*
+ * Creating an account
+ *
+ */
+        char firstname[40], lastname[50];
+
+        printf("Create an account \n");
+        printf("Enter your first and last names here: ");
+        scanf("%s %s", firstname, lastname);
+}
