4b6dea057f878db7689b180196460ebe378c2f77
Henry <ikegwuonuhenry25@gmail.com>
2023-12-19 20:11:14 +0000

    validated

--- user.h
+++ user.h
@@ -6,5 +6,6 @@ int validatePassword(const char* password);
 void transfer(double* balance, double* lastTransferAmount);
 void deposit(double* balance);
 void checkBalance(double balance);
+void printReceipt(const char* transactionType, const char* accountName, const char* accountNumber, const char* bank, double amount, double previousBalance, double currentBalance);
 
 #endif
